98f03c2572f6ef262ef41861fde06a87b8048a19
Lucas Cardoso <lucascardoso946@gmail.com>
2022-04-20 22:47:25 -0300

    Creating user seed

--- 02-projeto-Rent-a-Car/src/shared/infra/http/server.ts
+++ 02-projeto-Rent-a-Car/src/shared/infra/http/server.ts
@@ -1,16 +1,18 @@
 import "reflect-metadata";
-import express, { Request, Response, NextFunction } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
-import swaggerFile from "../../../swagger.json";
 
-import "@shared/infra/typeorm";
 import "@shared/container";
-
 import { AppError } from "@shared/errors/AppError";
-import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
 
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+createConnection();
 const app = express();
+
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
@@ -25,4 +27,5 @@ app.use(
     });
   }
 );
+
 app.listen(3333, () => console.log("SERVER ON!"));
