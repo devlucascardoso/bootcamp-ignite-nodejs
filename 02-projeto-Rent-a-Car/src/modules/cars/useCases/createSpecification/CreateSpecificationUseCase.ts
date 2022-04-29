5ab9bb7820eb7039619b074850b201f4515f0042
Lucas Cardoso <lucascardoso946@gmail.com>
2022-04-29 01:05:48 -0300

    feat: Add integration tests to categories

--- 02-projeto-Rent-a-Car/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ 02-projeto-Rent-a-Car/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { AppError } from "@shared/errors/AppError";
 
-import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 interface IRequest {
   name: string;
@@ -12,7 +12,7 @@ interface IRequest {
 class CreateSpecificationUseCase {
   constructor(
     @inject("SpecificationsRepository")
-    private specificationsRepository: ISpecificationRepository
+    private specificationsRepository: ISpecificationsRepository
   ) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
--- 02-projeto-Rent-a-Car/src/shared/infra/http/routes/categories.routes.ts
+++ 02-projeto-Rent-a-Car/src/shared/infra/http/routes/categories.routes.ts
@@ -4,8 +4,8 @@ import multer from "multer";
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
-import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
@@ -31,9 +31,7 @@ categoriesRoutes.post(
   upload.single("file"),
   ensureAuthenticated,
   ensureAdmin,
-  (request, response) => {
-    return importCategoryController.handle(request, response);
-  }
+  importCategoryController.handle
 );
 
 export { categoriesRoutes };
