49a1993db7a62dc86f0fa159350c26a92e31ba06
Lucas Cardoso <lucascardoso946@gmail.com>
2022-05-17 15:54:34 -0300

    fix: lint

--- 02-projeto-Rent-a-Car/jest.config.ts
+++ 02-projeto-Rent-a-Car/jest.config.ts
@@ -10,7 +10,7 @@ export default {
   bail: true,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\iagoc.SICOOB3169\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "C:\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
--- 02-projeto-Rent-a-Car/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ 02-projeto-Rent-a-Car/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository } from "typeorm";
 
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
 import { Rental } from "../entities/Rental";
 
 class RentalsRepository implements IRentalsRepository {
